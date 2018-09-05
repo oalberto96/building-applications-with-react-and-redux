515e456619c888a1b07971dda12b2c916fba367d
oalberto96 <ochoapublic@gmail.com>
2018-09-05 12:38:07 -0700

    Dispatch Thunk api

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -4,23 +4,6 @@ import { bindActionCreators } from "redux";
 import * as courseActions from "../../actions/courseActions";
 
 class CoursesPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { course: { title: "" } };
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course: course });
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
-  }
-
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
   }
@@ -30,13 +13,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1>Courses</h1>
         {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="save" onClick={this.onClickSave} />
       </div>
     );
   }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
-  return createStore(
-    rootReducer,
-    initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    //applyMiddleware(reduxImmutableStateInvariant())
-  );
+  return createStore(rootReducer, initialState, applyMiddleware(thunk));
 }
