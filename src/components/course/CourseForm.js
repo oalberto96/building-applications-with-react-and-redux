e891b540eedfc41183cde87d8be53777c53e40fa
oalberto96 <ochoapublic@gmail.com>
2018-09-07 13:45:35 -0700

    Create form submission loading indicator

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -7,7 +7,7 @@ const CourseForm = ({
   allAuthors,
   onSave,
   onChange,
-  loading,
+  saving,
   errors
 }) => {
   return (
@@ -49,8 +49,8 @@ const CourseForm = ({
 
       <input
         name="submit"
-        disabled={loading}
-        value={loading ? "Saving..." : "Save"}
+        disabled={saving}
+        value={saving ? "Saving..." : "Save"}
         className="btn btn-primary"
         onClick={onSave}
       />
@@ -63,7 +63,7 @@ CourseForm.propTypes = {
   allAuthors: React.PropTypes.array,
   onSave: React.PropTypes.func.isRequired,
   onChange: React.PropTypes.func.isRequired,
-  loading: React.PropTypes.bool,
+  saving: React.PropTypes.bool,
   errors: React.PropTypes.object
 };
 
