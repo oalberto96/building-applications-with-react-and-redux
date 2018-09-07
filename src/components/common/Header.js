24096b533f46725cd2c2ac9b60a0280b49978179
oalberto96 <ochoapublic@gmail.com>
2018-09-07 13:12:37 -0700

    Show AJAX status

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from "react";
 import { Link, IndexLink } from "react-router";
 import LoadingDots from "./LoadingDots";
 
-const Header = () => {
+const Header = ({ loading }) => {
   return (
     <nav>
       <IndexLink to="/" activeClassName="active">
@@ -16,9 +16,13 @@ const Header = () => {
       <Link to="/about" activeClassName="active">
         About
       </Link>
-      <LoadingDots interval={100} dots={20} />
+      {loading && <LoadingDots interval={100} dots={20} />}
     </nav>
   );
+};
+
+Header.propTypes = {
+  loading: PropTypes.bool.isRequired
 };
 
 export default Header;
