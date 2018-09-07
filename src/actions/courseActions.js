1379ba8da707c5f23f8ebd8703a874e3d6ef1a77
oalberto96 <ochoapublic@gmail.com>
2018-09-07 14:51:16 -0700

    Error Handling

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from "./actionTypes";
 import courseApi from "../api/mockCourseApi";
-import { beginAjaxCall } from "./ajaxStatusActions";
+import { beginAjaxCall, ajaxCallError } from "./ajaxStatusActions";
 
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
@@ -23,6 +23,7 @@ export function loadCourses() {
         dispatch(loadCoursesSuccess(courses));
       })
       .catch(error => {
+        dispatch(ajaxCallError());
         throw error;
       });
   };
@@ -39,6 +40,7 @@ export function saveCourse(course) {
           : dispatch(createCourseSuccess(savedCourse));
       })
       .catch(error => {
+        dispatch(ajaxCallError());
         throw error;
       });
   };
